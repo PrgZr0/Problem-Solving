38e5fc72e20489c2b65133e9c903f7b094faa33f
ProgZer0 <prog0mohamed@gmail.com>
2019-09-30 22:51:36 +0200

    hello,worldgit add *

---
+++ ACM-ICPC Problems Solutions/Solution of 12895 - Armstrong Number .py
@@ -0,0 +1,12 @@
+a=raw_input("Enter an Number :- ")
+ls=[]
+if (int(a)<=1000000000):
+   for i in range(0,len(a)):
+       ls.append(int(a[i]))
+       print(a[i])
+       b=len(a)
+       print(b)
+       
+else:
+    pass
+
