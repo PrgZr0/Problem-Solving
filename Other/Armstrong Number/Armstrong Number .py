22cbfd1f05bfa279f5c4e4418b0e66c21a1e805b
ProgZer0 <prog0mohamed@gmail.com>
2019-10-01 04:21:42 +0200

    Update..

--- Other/Armstrong Number/Armstrong Number .py
+++ Other/Armstrong Number/Armstrong Number .py
@@ -1,18 +1,17 @@
 import math
-a=raw_input("Enter an Number :- ")
-ls=[]
-b=0
-e=0
-if (int(a)<=1000000000):
-   for i in range(0,len(a)):
-       ls.append(int(a[i]))
-       b=len(a)
-else:
-    pass
-for i in range(0,len(ls)) :
-   e+=math.pow(ls[i], b)
-   
-if (e==int(a)):
-   print("Armstrong")
-else:
-   print("Not Armstrong")
+while(1):
+   a=raw_input("Enter an Number :- ")
+   ls=[]
+   b=0
+   e=0
+   if (int(a)<=1000000000):
+      for i in range(0,len(a)):
+          ls.append(int(a[i]))
+          b=len(a)
+          e+=math.pow(ls[i], b)
+      if (e==int(a)):
+         print("Armstrong")
+      else:
+         print("Not Armstrong")
+   else:
+       pass
