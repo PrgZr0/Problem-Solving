281cc86181c316dd171f4195df4d90397e16f553
M <nerd5000@nerd.com>
2019-10-03 23:03:41 +0200

    ds

---
+++ Other/Armstrong Number/Armstrong Number .py
@@ -0,0 +1,22 @@
+#@Anthor : Prog0Mohamed
+#@Date : 12:00 AM - 1/10/2019
+#@Info : Armstrong Number
+#Hacked From Nerd5000
+#happy hacking bitches :)
+import math
+while(1):
+   num=raw_input("Enter an Number :- ")
+   ls=[]
+   count=0
+   result=0
+   if (int(num)<=1000000000):
+      for i in range(0,len(num)):
+          ls.append(int(num[i]))
+          count=len(num)
+          result+=math.pow(ls[i], count)
+      if (result==int(num)):
+         print("Armstrong")
+      else:
+         print("Not Armstrong")
+   else:
+       pass
