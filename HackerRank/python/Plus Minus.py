11ae431bcd1c84fc9194bcbb2b8de0aa6eece81a
Muhammed AbdElrhman <nerd5000@nerd.com>
2019-10-04 21:07:29 +0200

    adding new problems

---
+++ HackerRank/python/Plus Minus.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    positive=0
+    negative=0
+    zero=0
+    for i in range(len(arr)):  
+        if arr[i]==0:
+            zero+=1
+        elif arr[i]==abs(arr[i]):
+            positive+=1
+        else:
+            negative+=1
+    print(positive/len(arr))
+    print(negative/len(arr))
+    print(zero/len(arr))
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
